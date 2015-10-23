b9a63ed19b683685d1cd3315e5b1f126852ebd7a
rcamp021 <rcamp021@odu.edu>
2015-10-23 16:41:42 -0400

    Added some models

---
+++ InSeasonAPI/Models/Hunting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InSeasonAPI.Models
+{
+    class Hunting
+    {
+        public string agency { get; set; }
+        public DateTime date_effective { get; set; }
+        public DateTime date_expires { get; set; }
+        public string documentation { get; set; }
+        public string schema_version { get; set; }
+        public string conformsTo { get; set; }
+        public string state { get; set; }
+        public List<Season> seasons {get; set;}
+        public Species species { get; set; }
+
+    }
+}
---
+++ InSeasonAPI/Models/Season.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InSeasonAPI.Models
+{
+    class Season
+    {
+        public string name { get; set; }
+        public string comment { get; set; }
+    
+    }
+}
