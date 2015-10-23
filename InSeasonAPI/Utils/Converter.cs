ce7a4b54ff0f48b80a9e4b8d53bb9cc100ada6e4
rcamp021 <rcamp021@odu.edu>
2015-10-23 18:05:39 -0400

    linq to csv usage

--- InSeasonAPI/Utils/Converter.cs
+++ InSeasonAPI/Utils/Converter.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToExcel;
+using System.IO;
 
 namespace InSeasonAPI.Utils
 {
     class Converter
     {
-        public int Convert()
+        public object Convert()
         {
-            return 1;
+           // using (StreamReader sr = new StreamReader(System.Web.HttpContext.Current.Server.MapPath("~/App_Data/crow.json")))
+           // {
+                var excel = new ExcelQueryFactory(System.Web.HttpContext.Current.Server.MapPath("~/App_Data/crow.json"));
+            //}
+            return (from x in excel.Worksheet<object>() select x).FirstOrDefault();
         }
     }
 }
