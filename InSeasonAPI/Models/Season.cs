dd9307803134e4aa5d4a4ff89329574ce7aa4586
rcamp021 <rcamp021@odu.edu>
2015-10-23 16:44:53 -0400

    Updated vs includes

--- InSeasonAPI/Models/Season.cs
+++ InSeasonAPI/Models/Season.cs
@@ -10,6 +10,8 @@ namespace InSeasonAPI.Models
     {
         public string name { get; set; }
         public string comment { get; set; }
-    
+        public List<string> method_rules { get; set; }
+        public List<string> method { get; set; }
+
     }
 }
