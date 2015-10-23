31d418a76767cbe192af5c960740fea340b1c7c7
John Spaetzel <john.spaetzel@gmail.com>
2015-10-23 18:39:58 -0400

    Fixed seasons

---
+++ InSeasonAPI/Models/Seasons.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InSeasonAPI.Models
+{
+    class Seasons
+    {
+        public string comment { get; set; }
+        public Season season { get; set; }
+    }
+}
