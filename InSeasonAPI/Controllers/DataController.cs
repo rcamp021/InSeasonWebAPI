dbab7f6525e4f10fb047939dee8f240dd10b23d7
rcamp021 <rcamp021@odu.edu>
2015-10-23 22:27:44 -0400

    Broken

--- InSeasonAPI/Controllers/DataController.cs
+++ InSeasonAPI/Controllers/DataController.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Web.Http;
 using InSeasonAPI.Models;
 using Newtonsoft.Json;
+using InSeasonAPI.Utils;
 
 namespace InSeasonAPI.Controllers
 {
@@ -25,12 +26,12 @@ namespace InSeasonAPI.Controllers
             using (StreamReader reader = new StreamReader(System.Web.HttpContext.Current.Server.MapPath(string.Format("~/App_Data/animals/{0}.json", animal))))
             {
                 Hunting animalobj= await System.Threading.Tasks.Task.Factory.StartNew(() => JsonConvert.DeserializeObject<Hunting>(reader.ReadToEnd()));
+                var seasons = animalobj.seasons.Select(x => x.range.Where(y => DateConverter.ConvertToDateTime(y.season.date.starts) >= date && DateConverter.ConvertToDateTime(y.season.date.ends) <= date)).FirstOrDefault();
 
-               // string[] splitdate = date.Split('-');
+                var conversion = new Utils.Converter();
+                var id = conversion.CountyToGnis(Convert.ToInt32(countyID));
 
-             //   string newdate = splitdate[]
-
-                var dates = animalobj.seasons.Select(x => x.range.Where(y => Convert.ToDateTime(y.season.date.starts) >= date && Convert.ToDateTime(y.season.date.ends) <= date));
+                var counties = seasons.Select(x => x.places.Where(y => y.Key == id.ForEach(z)));
                 return null;
             }
         }
