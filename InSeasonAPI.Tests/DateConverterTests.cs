0b67b99ca0febb3681a79194e3792153e55d6c05
John Spaetzel <john.spaetzel@gmail.com>
2015-10-23 22:10:26 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	InSeasonAPI/Controllers/DataController.cs
    
    Added Test project, added util for converting simple dates to datetime
    objects

---
+++ InSeasonAPI.Tests/DateConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using InSeasonAPI.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InSeasonAPI.Tests
+{
+    [TestClass]
+    public class DateConverterTests
+    {
+        [TestMethod]
+        public void DateConverterTestConvert()
+        {
+            var test = DateConverter.ConvertToDateTime("2014-12-01").ToString();
+            var result = new DateTime(2014, 12, 1, 23, 59, 59).ToString();
+
+            Assert.AreEqual(test, result);
+        }
+    }
+}
---
+++ InSeasonAPI/Utils/DateConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace InSeasonAPI.Utils
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class DateConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static DateTime ConvertToDateTime(string input)
+        {
+            string pattern = "yyyy-MM-dd";
+            DateTime parsedDate;
+            DateTime.TryParseExact(input, pattern, null,
+                DateTimeStyles.None, out parsedDate);
+            return parsedDate.AbsoluteEnd();
+        }
+        private static DateTime AbsoluteStart(this DateTime dateTime)
+        {
+            return dateTime.Date;
+        }
+
+        /// <summary>
+        /// Gets the 11:59:59 instance of a DateTime
+        /// </summary>
+        private static DateTime AbsoluteEnd(this DateTime dateTime)
+        {
+            return AbsoluteStart(dateTime).AddDays(1).AddTicks(-1);
+        }
+    }
+}
