a4d1145635472a0c46c74d04b1ace0c1353f52da
rcamp021 <rcamp021@odu.edu>
2015-10-23 21:33:35 -0400

    query

--- InSeasonAPI/Controllers/DataController.cs
+++ InSeasonAPI/Controllers/DataController.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using InSeasonAPI.Models;
+using Newtonsoft.Json;
 
 namespace InSeasonAPI.Controllers
 {
     public class DataController : ApiController
     {
-        //animal date, countyid
         /// <summary>
         /// 
         /// </summary>
         /// <param name="animal"></param>
+        /// <param name="date"></param>
+        /// <param name="countyID"></param>
         /// <returns></returns>
-        public IHttpActionResult Get(string animal)
+        [Route("GetCounty/{animal}/{date}/{countyID}")]
+        public async System.Threading.Tasks.Task<IHttpActionResult> Get(string animal, DateTime date, string countyID)
         {
-            return null;
+            using (StreamReader reader = new StreamReader(System.Web.HttpContext.Current.Server.MapPath(string.Format("~/App_Data/animals/{0}.json", animal))))
+            {
+                Hunting animalobj= await System.Threading.Tasks.Task.Factory.StartNew(() => JsonConvert.DeserializeObject<Hunting>(reader.ReadToEnd()));
+
+             //   string newDate = Convert.ToDateTime(date);
+
+                var dates = animalobj.seasons.season.range.Where(x => Convert.ToDateTime(x.season.date.starts) >= date &&
+                                                                    Convert.ToDateTime(x.season.date.ends) <= date);
+                return null;
+            }
         }
     }
 }
