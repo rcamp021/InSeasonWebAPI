cb32f306ce8172a504b89dedb152a248d46cb460
John Spaetzel <john.spaetzel@gmail.com>
2015-10-23 20:51:17 -0400

    Changed CountyToGnis to return a list

--- InSeasonAPI/Utils/Converter.cs
+++ InSeasonAPI/Utils/Converter.cs
@@ -19,9 +19,9 @@ namespace InSeasonAPI.Utils
             return (from x in excelQueryFactory.Worksheet<GnisCountyDefinition>() where x.FEATURE_ID == gnis select x).FirstOrDefault();
         }
 
-        public GnisCountyDefinition CountyToGnis(int county)
+        public List<GnisCountyDefinition> CountyToGnis(int county)
         {
-            return (from x in excelQueryFactory.Worksheet<GnisCountyDefinition>() where x.COUNTY_NUMERIC == county select x).FirstOrDefault();
+            return (from x in excelQueryFactory.Worksheet<GnisCountyDefinition>() where x.COUNTY_NUMERIC == county select x).ToList();
         }
 
     }
